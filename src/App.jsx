d5ff77e8be115c090daf4e50ee59cf0aa318278d
Mihail Gaberov <mihail.gaberov@gmail.com>
2022-05-15 12:38:39 +0200

    :construction: Introducing routes - cart page, detail page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import "./App.css";
 import Footer from "./Footer";
 import Header from "./Header";
 import Products from "./Products";
+import { Routes, Route } from "react-router-dom";
+import Detail from "./Detail";
+import Cart from "./Cart";
 
 export default function App() {
   return (
@@ -10,7 +13,11 @@ export default function App() {
       <div className="content">
         <Header />
         <main>
-          <Products />
+          <Routes>
+            <Route path="/" element={<Products />}></Route>
+            <Route path="/detail" element={<Detail />}></Route>
+            <Route path="/cart" element={<Cart />}></Route>
+          </Routes>
         </main>
       </div>
       <Footer />
