dfe2436b78bc8d3fe405699e28e683f45f1461b7
mihail.gaberov <mihail.gaberov@ysura.com>
2022-05-19 12:50:26 +0200

    👷🏻‍♂️  Building Checkout form

--- src/Checkout.jsx
+++ src/Checkout.jsx
@@ -1,5 +1,12 @@
 import React, { useState } from "react";
 
+const STATUS = {
+  IDLE: "IDLE",
+  SUBMITTED: "SUBMITTED",
+  SUBMITTING: "SUBMITTING",
+  COMPLETED: "COMPLETED",
+};
+
 const emptyAddress = {
   city: "",
   country: "",
@@ -7,6 +14,7 @@ const emptyAddress = {
 
 export default function Checkout({ cart }) {
   const [address, setAddress] = useState(emptyAddress);
+  const [status, setStatus] = useState(STATUS.IDLE);
 
   function handleChange(e) {
     e.persist();
@@ -23,7 +31,8 @@ export default function Checkout({ cart }) {
   }
 
   async function handleSubmit(event) {
-    // TODO
+    event.preventDefault();
+    setStatus(STATUS.SUBMITTING);
   }
 
   return (
@@ -64,6 +73,7 @@ export default function Checkout({ cart }) {
             type="submit"
             className="btn btn-primary"
             value="Save Shipping Info"
+            disabled={status === STATUS.SUBMITTING}
           />
         </div>
       </form>
