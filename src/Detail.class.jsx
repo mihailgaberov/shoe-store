5b2423b7079133811a9acd0deb74e43b989f6370
Mihail Gaberov <mihail.gaberov@gmail.com>
2022-05-22 10:27:23 +0200

    :sparkles: Consume multiple contexts in a class component

--- src/Detail.class.jsx
+++ src/Detail.class.jsx
@@ -17,53 +17,60 @@ class Detail extends React.Component {
     sku: [],
   };
 
-  static contextType = CartContext;
-
   render() {
     const { id, navigate } = this.props;
     const { sku } = this.state;
 
     return (
-      <Fetch url={`products/${id}`}>
-        {(product, loading, error) => {
-          if (loading) return <Spinner />;
-          if (!product) return <PageNotFound />;
-          if (error) throw error;
-
+      <CartContext.Consumer>
+        {({ dispatch }) => {
           return (
-            <div id="detail">
-              <h1>{product.name}</h1>
-              <p>{product.description}</p>
-              <p id="price">${product.price}</p>
-              <select
-                id="size"
-                value={sku}
-                onChange={(e) => this.setState({ sku: e.target.value })}
-              >
-                <option value="">What size?</option>
-                {product.skus.map((s) => (
-                  <option key={s.sku} value={s.sku}>
-                    {s.size}
-                  </option>
-                ))}
-              </select>
-              <p>
-                <button
-                  disabled={!sku}
-                  className="btn btn-primary"
-                  onClick={() => {
-                    this.context.dispatch({ type: "add", id, sku });
-                    return navigate("/cart");
-                  }}
-                >
-                  Add to cart
-                </button>
-              </p>
-              <img src={`/images/${product.image}`} alt={product.category} />
-            </div>
+            <Fetch url={`products/${id}`}>
+              {(product, loading, error) => {
+                if (loading) return <Spinner />;
+                if (!product) return <PageNotFound />;
+                if (error) throw error;
+
+                return (
+                  <div id="detail">
+                    <h1>{product.name}</h1>
+                    <p>{product.description}</p>
+                    <p id="price">${product.price}</p>
+                    <select
+                      id="size"
+                      value={sku}
+                      onChange={(e) => this.setState({ sku: e.target.value })}
+                    >
+                      <option value="">What size?</option>
+                      {product.skus.map((s) => (
+                        <option key={s.sku} value={s.sku}>
+                          {s.size}
+                        </option>
+                      ))}
+                    </select>
+                    <p>
+                      <button
+                        disabled={!sku}
+                        className="btn btn-primary"
+                        onClick={() => {
+                          dispatch({ type: "add", id, sku });
+                          return navigate("/cart");
+                        }}
+                      >
+                        Add to cart
+                      </button>
+                    </p>
+                    <img
+                      src={`/images/${product.image}`}
+                      alt={product.category}
+                    />
+                  </div>
+                );
+              }}
+            </Fetch>
           );
         }}
-      </Fetch>
+      </CartContext.Consumer>
     );
   }
 }
