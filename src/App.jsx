b5f47bbfce52899aeb19846469d7ce5978e1495b
Mihail Gaberov <mihail.gaberov@gmail.com>
2022-05-22 08:58:41 +0200

    :sparkles: Use custom hook to levarage React context

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,16 @@
-import React, { useReducer, useEffect } from "react";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Cart from "./Cart";
+import Checkout from "./Checkout";
+import Detail from "./Detail";
 import Footer from "./Footer";
 import Header from "./Header";
 import Products from "./Products";
-import { Routes, Route } from "react-router-dom";
-import Detail from "./Detail";
-import Cart from "./Cart";
-import Checkout from "./Checkout";
-import cartReducer from "./cartReducer";
-import { CartContext } from "./cartContext";
-
-let initialCart;
-
-try {
-  initialCart = JSON.parse(localStorage.getItem("cart")) ?? [];
-} catch {
-  console.error("The cart could not be parsed into JSON.");
-  initialCart = [];
-}
 
 export default function App() {
-  const [cart, dispatch] = useReducer(cartReducer, initialCart);
-
-  useEffect(() => localStorage.setItem("cart", JSON.stringify(cart)), [cart]);
-
   return (
-    <CartContext.Provider value={{ cart, dispatch }}>
+    <>
       <div className="content">
         <Header />
         <main>
@@ -34,19 +19,13 @@ export default function App() {
               <h1>Welcome to Carved Rock Fitness</h1>
             </Route>
             <Route path="/:category" element={<Products />}></Route>
-            <Route
-              path="/:category/:id"
-              element={<Detail dispatch={dispatch} />}
-            ></Route>
+            <Route path="/:category/:id" element={<Detail />}></Route>
             <Route path="/cart" element={<Cart />}></Route>
-            <Route
-              path="/checkout"
-              element={<Checkout cart={cart} dispatch={dispatch} />}
-            ></Route>
+            <Route path="/checkout" element={<Checkout />}></Route>
           </Routes>
         </main>
       </div>
       <Footer />
-    </CartContext.Provider>
+    </>
   );
 }
--- src/cartContext.js
+++ src/cartContext.js
@@ -1,3 +1,33 @@
-import React from "react";
+import React, { useReducer, useEffect, useContext } from "react";
+import cartReducer from "./cartReducer";
 
-export const CartContext = React.createContext(null);
+const CartContext = React.createContext(null);
+
+let initialCart;
+
+try {
+  initialCart = JSON.parse(localStorage.getItem("cart")) ?? [];
+} catch {
+  console.error("The cart could not be parsed into JSON.");
+  initialCart = [];
+}
+
+export function CartProvider(props) {
+  const [cart, dispatch] = useReducer(cartReducer, initialCart);
+  useEffect(() => localStorage.setItem("cart", JSON.stringify(cart)), [cart]);
+
+  const contextValue = {
+    cart,
+    dispatch,
+  };
+  return (
+    <CartContext.Provider value={contextValue}>
+      {props.children}
+    </CartContext.Provider>
+  );
+}
+
+export function useCart() {
+  const context = useContext(CartContext);
+  return context;
+}
