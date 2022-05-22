de4c40b4bf9e25bb8a64b67e626a5cc82d034c02
Mihail Gaberov <mihail.gaberov@gmail.com>
2022-05-22 10:01:24 +0200

    :sparkles: Converted Checkout to class component

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Cart from "./Cart";
-import Checkout from "./Checkout";
+import Checkout from "./Checkout.class";
 import Detail from "./Detail";
 import Footer from "./Footer";
 import Header from "./Header";
 import Products from "./Products";
+import { useCart } from "./cartContext";
 
 export default function App() {
+  const { dispatch } = useCart();
   return (
     <>
       <div className="content">
@@ -21,7 +23,10 @@ export default function App() {
             <Route path="/:category" element={<Products />}></Route>
             <Route path="/:category/:id" element={<Detail />}></Route>
             <Route path="/cart" element={<Cart />}></Route>
-            <Route path="/checkout" element={<Checkout />}></Route>
+            <Route
+              path="/checkout"
+              element={<Checkout dispatch={dispatch} />}
+            ></Route>
           </Routes>
         </main>
       </div>
